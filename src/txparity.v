ea2ee28195597fe55a37ad1dc402e128ff155b32
Traffipax <Traffipax@users.noreply.github.com>
2019-05-13 16:17:43 +0200

    Update txparity.v

--- src/txparity.v
+++ src/txparity.v
@@ -11,14 +11,14 @@ module txparity ( //ParityGen
   integer count = 0;
   integer i = 1;
 
-  always @ (posedge i_Pclk)
+  always @ (posedge i_Pclk, i_Parity)
 	begin
     	count = 0;
       
       for(i=0; i<=7; i=i+1)
     	begin
           if(i_Data[i]==1)
-        	count <= count + 1;
+        	count = count + 1;
 	  	end
       
       case (i_Parity)
@@ -38,7 +38,7 @@ module txparity ( //ParityGen
         	paritybit <= 0;
       endcase
       
-      o_Data <= {stopbit,paritybit,i_Data[7:0],startbit };
+      o_Data <= {startbit,i_Data[7:0],paritybit,stopbit };
     end
 
 
